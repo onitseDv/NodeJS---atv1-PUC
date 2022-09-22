2bc3a0525867f9e6c695a31d0b30a39022d971e0
onitseDv <onitsedv@hotmail.com>
2022-09-22 13:23:19 -0300

    commit

---
+++ Rotas/rotas.js
@@ -0,0 +1,119 @@
+
+const express = require ('express');
+const rotas = express.Router(); //biblioteca router do express
+
+const itemProduto = require ('../data/produtos.json'); //informações
+
+module.exports = rotas;
+
+//teste: http://localhost:3000/produtos/
+rotas.get('/produtos', (requisicao, resposta) => {
+    return resposta.json(itemProduto)
+    //return resposta.json({itemProduto})
+});
+
+
+//teste: http://localhost:3000/produtos/id-aqui
+rotas.get('/produtos/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+    for(let produto_search of itemProduto){
+        if(produto_search.id === id){
+            resposta.json(produto_search)
+            return
+        }
+    }
+    resposta.status(404).send('Produto não encontrado!')
+})
+
+
+//teste: POST pelo postman com o endpoint: http://localhost:3000/produtos
+//       com a mesma estrutura que o array possui
+/* outra maneira de encontrar o max id. da erro ao exibir resposta.send(max) pq nao é obj json
+    const ids = itemProduto.map(object => {
+        return object.id;
+    });  
+    //resposta.send(ids)
+    const max = Math.max(...ids);
+    console.log(max)
+*/
+rotas.post('/produtos/adicionarProduto', (requisicao, resposta) => {
+    const body = requisicao.body
+    if(!body.descricao){
+        return resposta.status(400).end('Descrição do produto não pode ser nula!')
+    }else if(!body.valor){
+        return resposta.status(400).end('Valor do produto não pode ser nulo!')
+    }else if(!body.marca){
+        return resposta.status(400).end('Marca do produto não pode ser nula!')
+    }else{
+        //verificar se o produto existe, caso o retorno for nulo, insere
+        const result = itemProduto.find( desc => desc.descricao === body.descricao)
+        if(!result){
+            const maxID = itemProduto.reduce(function(prev, current) { 
+                return prev.id > current.id ? prev : current; 
+            })
+            //console.log(maxID.id)
+            const obj = {
+                id: maxID.id + 1,
+                descricao: body.descricao,
+                valor: body.valor,
+                marca: body.marca
+            }
+            //resposta.send(obj)
+            itemProduto.push(obj)
+            resposta.send(`Produto ${requisicao.body.descricao} adicionado`)
+        }else{
+            resposta.status(400).end(`Produto ${requisicao.body.descricao} já existente`)
+        }
+    }
+});
+ 
+//ta com erro no PUTTTTTTTTTTTTTTTTTTTTTTTT
+//teste: http://localhost:3000/produtos/alteraProduto/id-aqui  
+rotas.put('/produtos/alteraProduto/:id', (requisicao, resposta) => {
+    const body = requisicao.body
+    //if(!body.id){
+    //    return resposta.status(400).end('ID do produto não pode ser nulo!')
+    if(!body.descricao){
+        return resposta.status(400).end('Descrição do produto não pode ser nula!')
+    }else if(!body.valor){
+        return resposta.status(400).end('Valor do produto não pode ser nulo!')
+    }else if(!body.marca){
+        return resposta.status(400).end('Marca do produto não pode ser nula!')
+    }else{
+        //verificar se o produto existe para alterar
+        const result = itemProduto.find( idProd => idProd.id === requisicao.params.id)
+        if(!result){
+            return resposta.status(400).end(`Produto com id ${requisicao.params.id} não encontrado`)
+        }else{
+            //itemProduto[requisicao.params.id - 1].id = requisicao.body.id;
+            itemProduto[requisicao.params.id - 1].descricao = requisicao.body.descricao;
+            itemProduto[requisicao.params.id - 1].valor = requisicao.body.valor;
+            itemProduto[requisicao.params.id - 1].marca = requisicao.body.marca;
+            return resposta.json(itemProduto[requisicao.params.id - 1]);
+        }
+    }
+})
+
+
+/*rotas.delete('/times/delete/:id', (requisicao, resposta) =>{
+    const identificador = requisicao.params.id
+    let newItem = itemTime.filter(item => {
+        if(!item[identificador]){
+            return (item)
+        }
+    })
+    itemTime = newItem
+    return resposta.send(newItem)
+    
+    
+    /*itemTime = itemTime.filter(itemTime => {
+        if(itemTime.id !== id){
+            return true
+       }
+       return false
+    })
+    resposta.send(`Time com id ${id} excluído!`)
+    
+}); */
+
+
